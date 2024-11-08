a1808a7f67a2065c4431c4f3ad5e67fe9d9dfc29
Soroush <s.ghodrat@gmail.com>
2024-11-08 13:45:36 +0100

    Add i18next for internationalization and update app structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import DataTable from "./components/DataTable";
 import useTheme from "./hooks/useTheme";
 import Navbar from "./components/Navbar";
 import { LanguageProvider } from "./context/LanguageContext";
+import { Suspense } from "react";
 
 const App: React.FC = () => {
   const theme = useTheme();
 
   return (
-    <ThemeProvider theme={theme}>
-      <LanguageProvider>
-        <CssBaseline />
-        <Navbar />
-        <DataTable />
-      </LanguageProvider>
-    </ThemeProvider>
+    <Suspense fallback={<div>"Loading default language..."</div>}>
+      <ThemeProvider theme={theme}>
+        <LanguageProvider>
+          <CssBaseline />
+          <Navbar />
+          <DataTable />
+        </LanguageProvider>
+      </ThemeProvider>
+    </Suspense>
   );
 };
 
---
+++ src/i18n.ts
@@ -0,0 +1,34 @@
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n.use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init(
+        {
+            lng: "en",
+            backend: {
+                //translation file path
+                loadPath: "/locales/{{lng}}/{{ns}}.json",
+            },
+            //if any of languages not chosen, english will be chose as default
+            fallbackLng: "en",
+            //allow to look at all the languages
+            debug: true,
+            ns: ["navbar"],
+            defaultNS: "navbar",
+            interpolation: {
+                escapeValue: false,
+                formatSeparator: ",",
+            },
+        },
+        (error) => {
+            if (error) {
+                return console.log("Loading i18n error", error);
+            }
+        }
+    );
+
+export default i18n;
