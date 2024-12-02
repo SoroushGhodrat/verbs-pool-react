d0c12032d87abc79d174fda9f16a1cba87e7be7e
Soroush <s.ghodrat@gmail.com>
2024-12-02 15:11:00 +0100

    Enhance randonColor function to support opacity and return RGBA format

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -2,8 +2,14 @@ export const goToTop = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
-export const randonColor = () => {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+export const randonColor = (opacity: number = 0.1) => {
+  const color = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0');
+  const alpha = Math.round(opacity * 255)
+    .toString(16)
+    .padStart(2, '0');
+  return `#${color}${alpha}`;
 };
 
 export const generateAlphabet = (
